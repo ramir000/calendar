c2b72e292547a60bce948cd506a4cdc163431ba4
rgentile@qsocialnow.com <rgentile@qsocialnow.com>
2023-12-15 11:09:24 -0300

    Added calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,68 @@ import { EventService } from './services/event.service';
 })
 export class AppComponent implements OnInit {
   @ViewChild("scheduler_here", { static: true }) schedulerContainer!: ElementRef;
+  // define configs
+  compactHeader = {
+    rows: [
+      {
+        cols: [
+          "prev",
+          "date",
+          "next",
+        ]
+      },
+      {
+        cols: [
+          "month",
+          "week",
+          "agenda",
+          "spacer",
+          "today"
+        ]
+      }
+    ]
+  };
 
-  constructor(private eventService: EventService) { }
+  fullHeader = [
+    "day",
+    "week",
+    "month",
+    "date",
+    "prev",
+    "today",
+    "next"
+  ];
+
+  constructor(private eventService: EventService) {
+    this.resetConfig = this.resetConfig.bind(this)
+  }
   ngOnInit() {
     scheduler.plugins({
       agenda_view: true
     });
-    scheduler.config.date_format = "%Y-%m-%d %H:%i";
-    scheduler.init(this.schedulerContainer.nativeElement, new Date(2023, 12, 15), "week");
+    this.resetConfig();
+    scheduler.attachEvent("onBeforeViewChange", this.resetConfig);
+    scheduler.attachEvent("onSchedulerResize", this.resetConfig);
+    scheduler.i18n.setLocale("es");
+    scheduler.config.responsive_lightbox = true; // responsive lightbox
+    scheduler.config.date_format = "%Y-%n-%j %H:%i";
+    scheduler.init(this.schedulerContainer.nativeElement, new Date(2023, 12, 15), "agenda");
     this.eventService.get()
       .then((data) => {
         scheduler.parse(data);
       });
   }
+
+  resetConfig() {
+    let header;
+    if (window.innerWidth < 1000) {
+      header = this.compactHeader;
+    } else {
+      header = this.fullHeader;
+    }
+    scheduler.config.header = header;
+    return true;
+  }
+
 
 }
--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Event } from "../models/event";
 import * as moment from 'moment';
-
+import * as momentRange from 'moment-range';
+const moments = momentRange.extendMoment(moment);
 @Injectable()
 export class EventService {
   steps = [24, 72]
+  color = ['#dc5faf', '#43a743']
   calc() {
-    let date = moment("2023-12-15 6:45", "YYYY-MM-DD hh:mm")
-    const dias: Event[] = []
+    let date = moment("2023-12-15 6:45", "YYYY-MM-DD HH:mm")
+    let dias: Event[] = []
     for (let index = 0; index < 60; index++) {
-      dias.push({
-        "id": dias.length,
-        "text": this.steps[index % 2] == 24 ? "trabajo" : "descanzo",
-        "start_date": date.format("YYYY-MM-DD hh:mm"),
-        "end_date": date.add(this.steps[index % 2], 'hours').format("YYYY-MM-DD hh:mm")
+      let d = this.divideDate(
+        index,
+        this.steps[index % 2] == 24 ? "trabajo" : "descanzo",
+        date.format("YYYY-MM-DD HH:mm"),
+        date.add(this.steps[index % 2], 'hours').format("YYYY-MM-DD HH:mm"),
+        this.color[index % 2]
+      )
+      d.forEach((e, index) => {
+        e.id = dias.length + index + Math.random() * 10
+        dias.push(e)
       })
-
     }
-    console.log(dias)
     return dias
   }
+  
   get(): Promise<Event[]> {
     return Promise.resolve(this.calc());
+  }
+
+  divideDate(id: any, text: string, start: any, end: any, color:any) {
+    const result = []
+    let currentDate = moment(start).format("YYYY-MM-DD HH:mm")
+    let rRange = moment(start).endOf('day').format("YYYY-MM-DD HH:mm")
+    while (moment(currentDate, "YYYY-MM-DD HH:mm").isBefore(moment(end))) {
+      let range = {
+        "id": id,
+        "text": text,
+        "start_date": currentDate,
+        "end_date": rRange,
+        color,
+        textColor : 'white'
+      }
+      currentDate = moment(rRange).endOf('day').add(1, 'minutes').format("YYYY-MM-DD HH:mm")
+      rRange = moment(rRange, "YYYY-MM-DD HH:mm").endOf('day').add(1, 'minutes').endOf('day').format("YYYY-MM-DD HH:mm")
+      result.push(range)
+    }
+    result[result.length - 1].end_date = end
+    return result
   }
 }
